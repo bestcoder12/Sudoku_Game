dcfefdd04e544c9a53a9a0e85fe2c8aabcb20e01
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-09-28 23:03:32 +0530

    AsimpleHelloToTheUsers:)

---
+++ hello.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int i;
+	cout<<"Enter 1 for hello"<<endl;
+	cin>>i;
+	
+	if(i==1)
+	{
+		cout<<"Hello"<<endl;
+		
+	}
+	else
+	{
+		cout<<"Bye"<<endl;
+	}
+	
+	return 0;
+	
+	
+}

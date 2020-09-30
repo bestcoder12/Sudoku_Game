526531117cf9c58358e83e9bdce73f1e95afcca8
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-09-30 22:19:51 +0530

    added main function

---
+++ main-test.cpp
@@ -0,0 +1,21 @@
+#include "sudoku_class.h"
+#ifndef SUDOKU_CLASS_H 
+#define
+
+using namespace std;
+
+/*main function for calling the functions
+int main()
+{
+ Sudoku s;
+ 
+ 
+ /* calling the functions sudoku,input and print */
+ 
+ s.Sudoku();
+ s.input();
+ s.print_board();
+ 
+	
+	
+}

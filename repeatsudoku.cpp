cfb8d13efa2a52960f7992f21ff0f863bffb2a63
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-10-02 08:57:48 +0530

    Update repeatsudoku.cpp

--- repeatsudoku.cpp
+++ repeatsudoku.cpp
@@ -1,19 +1,27 @@
+/* Function Repeat to check if the the numbers present in the sudoku are not repeating */
+
 void sudoku:: repeat()
 {
 	int l,k,s;
 	
 	int count=0;
 	
+	
 	for(l=0;l<9;l++)
 	{
 		for(k=0;k<9;k++)
 		{
+			/*loop to add numbers from 0-9 to the each row and column for comparing the elements are repeating or not */
+			
 			for(s=0;s<9;s++)
 			{
 			   if(board[l][k]== board[l+s][k] || board[l][k]== board[l][k+s])
-			   count++;
+			   {
+				   if(l==(l+s) || k== (k+s))
+			            count++;
+			   }   
 			   
-			   if(count>1)
+			   else if(count>1)
 			   cout<<"Number is Repeating"<endl;
 			   break;
 			   

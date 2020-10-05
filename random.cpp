da6259e63563bb76252255ad75669be3eb2473e5
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-10-05 22:32:38 +0530

    Updated with random locations for Random numbers

--- random.cpp
+++ random.cpp
@@ -1,28 +1,44 @@
+/* function to enter random values at random locations in sudoku*/
+
 int random()
   
 {
   srand((unsigned)time(0));
-  int random_no;
-  int filled[9][9];
-  for(h=0;h<9;h++)
+  
+  int random_no1;
+  int f,z;
+  int filled_no[9][9];
+ 
+  for(s=0;s<filled;s++)
   {
-    random_no=(rand()%9)+1;
-    
-    for(l=0;i<9;i++)
+    for(p=0;p<row_lim;p++)
     {
-      for(k=0;j<9;j++)
+      for(u=0;u<col_lim;u++)
       {
-        
-        if(board[i][j]!=filled[l][k]))
-        {
-          if(i <=row_lim || j<=col_num )
-          {
-              filled[i][j]=random_no;
-          }
-        
-      } 
-      
+         for(h=0;h<9;h++)
+         {
+           
+           random_no1=(rand()%9)+1;
+           
+           /* f and z gets the random values from 0 to 9  
+           for getting random positions so that a random number gets placed at random location */
+           
+           f=rand()%9;
+           z=rand()%9;
+           
+           
+           filled_no[f][z]=random_no;
+         }
+      }
     }
   }
+}
+        
+        
+           
+           
+           
+           
+ 
     
     

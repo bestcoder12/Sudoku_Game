04a3a08b15c9f28ff9456ae6d3222cf118823e5d
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-10-05 09:46:39 +0530

    created random function

--- random.cpp
+++ random.cpp
@@ -1,1 +1,23 @@
 int random()
+  
+{
+  srand((unsigned)time(0));
+  int random_no;
+  int filled[9][9];
+  for(h=0;h<9;h++)
+  {
+    random_no=(rand()%9)+1;
+    
+    for(i=0;i<9;i++)
+    {
+      for(j=0;j<9;j++)
+      {
+        
+        filled[i][j]=random_no;
+        
+      } 
+      
+    }
+  }
+    
+    

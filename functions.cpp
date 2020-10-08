288baf05f2f548b3bb3d438423754ce3410c5e51
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-10-08 12:32:25 +0530

    Updated with RANDOM FUNCTION

--- functions.cpp
+++ functions.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string> 
+#include <ctime>
+#include<cstdlib>
 
 /* Header file for definition of the class Sudoku */
 #include "sudoku_class.h"
@@ -61,7 +63,33 @@ Sudoku::Sudoku()
 			board[i][j] = -1; 
 		}
 	}
-}		
+}
+
+
+
+
+void Sudoku::random()
+  
+{
+  srand((unsigned)time(0));
+  
+  int random_no;
+ 
+  for(s=0;s<filled;s++)
+  {
+    for(p=0;p<row_lim;p++)
+    {
+      for(u=0;u<col_lim;u++)
+      {
+        random_no=(rand()%9)+1;
+        f=rand()%9;
+        z=rand()%9;
+           
+        board[f][z]=random_no;
+         }
+      }
+    }
+  }
 		
 
 /* Input function for getting the row ,column and the element*/

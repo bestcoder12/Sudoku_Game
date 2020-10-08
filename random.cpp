749ac9549fc9a74f6538fdcfe7bc60df92fba721
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-10-08 11:45:41 +0530

    Update random.cpp

--- random.cpp
+++ random.cpp
@@ -1,13 +1,9 @@
-/* function to enter random values at random locations in sudoku*/
-
 int random()
   
 {
   srand((unsigned)time(0));
   
-  int random_no1;
-  int f,z;
-  int filled_no[9][9];
+  int random_no;
  
   for(s=0;s<filled;s++)
   {
@@ -15,24 +11,16 @@ int random()
     {
       for(u=0;u<col_lim;u++)
       {
-         for(h=0;h<9;h++)
-         {
+        random_no=(rand()%9)+1;
+        f=rand()%9;
+        z=rand()%9;
            
-           random_no1=(rand()%9)+1;
-           
-           /* f and z gets the random values from 0 to 9  
-           for getting random positions so that a random number gets placed at random location */
-           
-           f=rand()%9;
-           z=rand()%9;
-           
-           
-           filled_no[f][z]=random_no;
+        board[f][z]=random_no;
          }
       }
     }
   }
-}
+
         
         
            

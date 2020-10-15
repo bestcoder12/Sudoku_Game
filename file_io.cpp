f7c39af0094edf12a1fc00148321a2762a3e74ad
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-10-15 12:08:22 +0530

    file_io for printing details from the file

---
+++ file_io.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    fstream filename;
+    string message="Welcome to the Sudoku Game";
+    string name;
+
+
+    filename.open("Sudoku_file.txt",ios::out | ios::in);
+
+    if(!filename)
+    {
+        cout<<"No File is Created "<<endl;
+    }
+
+    cout<<"File created successfully "<<endl;
+    filename<<message<<endl;
+    filename>>message;
+    cout<<"Enter the Player Name "<<endl;
+    getline(cin,name);
+    filename<<name<<endl;
+    filename>>name;
+
+    cout<<"Welcome "<<name<<endl;
+
+    cout<<message<<endl;
+    
+    
+
+
+    filename.close();
+
+    return 0;
+
+}

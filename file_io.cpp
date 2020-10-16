2ccb82c1c709c70ff10fc997839a95e2ec8cf684
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-10-16 23:33:06 +0530

    Update file_io.cpp

--- file_io.cpp
+++ file_io.cpp
@@ -3,12 +3,16 @@
 #include<string>
 
 using namespace std;
+using namespace user_gm;
+
 
 int main()
 {
     fstream filename;
     string message="Welcome to the Sudoku Game";
     string name;
+    string highscore;
+    string message1;
 
 
     filename.open("Sudoku_file.txt",ios::out | ios::in);
@@ -18,19 +22,49 @@ int main()
         cout<<"No File is Created "<<endl;
     }
 
-    cout<<"File created successfully "<<endl;
-    filename<<message<<endl;
+    filename<<message;
     filename>>message;
     cout<<"Enter the Player Name "<<endl;
     getline(cin,name);
     filename<<name<<endl;
     filename>>name;
 
-    
     cout<<message<<endl;
-    cout<<"Your Name "<<name<<endl;
+    cout<<"Your Name : "<<name<<endl;
+
+    highscore=900-diff;
+
+    if(highscore<0)
+    {
+        filename<<"You already crossed the Time limit i.e., 900 seconds"<<endl;
+        filename>>"You already crossed the Time limit i.e., 900 seconds"<<endl;
+    }
+    else if(highscore>450 && highscore<800)
+    {
+        message1="GOOD";
+        filename<<message1;
+        filename>>message1;
+
+    }
+    else if(highscore>800)
+    {
+        message1="Outstanding";
+        filename<<message1;
+        filename>>message1;
+
+    }
+    else if(highscore<200)
+    {
+        message1="Bad";
+        filename<<message1;
+        filename>>message;
+    }
+    
+
 
     filename.close();
+
+    
 
     return 0;
 

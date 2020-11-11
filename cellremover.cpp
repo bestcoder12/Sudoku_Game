99ff64de7615ca19366940d37c584f4418d1af6a
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-11-11 23:55:37 +0530

    Converted filled into unfilled

--- cellremover.cpp
+++ cellremover.cpp
@@ -1,1 +1,14 @@
-#include<iostream>
+
+  default_random_engine defEngine(time(0));
+  uniform_int_distribution<int> intDistro(1,9);
+  vector<int> numbers;
+   
+    int c,r;
+
+for(int i=0;i<81-filled;i++)
+{
+  c= numbers.push_back(intDistro(defEngine));
+  r= numbers.push_back(intDistro(defEngine));
+  
+  board[c][r]=-1;
+}

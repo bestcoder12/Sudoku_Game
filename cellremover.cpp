96649690b8262c7368df081f73d9b687cdfa2ba4
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-11-13 15:46:00 +0530

    Update cellremover.cpp

--- cellremover.cpp
+++ cellremover.cpp
@@ -1,16 +1,15 @@
 void cell_remover()
 {
   default_random_engine defEngine(time(0));
-  uniform_int_distribution<int> intDistro(1,9);
+  uniform_int_distribution<int> intDistro(0,8);
   
-  int numbers;
-   
-    int c,r;
+ 
+      int c,r;
 
 for(int i=0;i<81-filled;i++)
 {
-  c= numbers.push_back(intDistro(defEngine));
-  r= numbers.push_back(intDistro(defEngine));
+  c= intDistro(defEngine);
+  r= intDistro(defEngine);
   
   board[c][r]=-1;
 }

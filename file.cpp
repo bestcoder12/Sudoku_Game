ce58d12922881718a47e2ed7da3d9284ed559203
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-11-21 00:05:01 +0530

    Updated file.cpp

--- file.cpp
+++ file.cpp
@@ -7,10 +7,9 @@
 using namespace std;
 using namespace user_gm;
 
-
-
-void Player::player_input(double diff)
+class file
 {
+    public:
     fstream filename;
     string message="Welcome to the Sudoku Game";
     string name;
@@ -19,47 +18,52 @@ void Player::player_input(double diff)
     string message2="WON,LOST,HIGHSCORE";
     int won=0;
     int lost=0;
+    void player_input();
+    void recall();
+    
+};
+void file::player_input()
+{
+   
+  filename.open("Sudoku_file.txt",ios::app | ios::in | ios::out);
 
-
-    filename.open("Sudoku_file.txt", ios::out | ios::app | ios::in);
-
-    if(! filename)
+    if(!filename)
     {
-        cout << "No File is Created " << endl;
-        return;
+        cout<<"No File is Created "<<endl;
     }
 
-    filename << message;
+    filename<<message;
     filename>>message;
+
+    if(filename)
     cout<<"Enter the Player Name "<<endl;
     getline(cin,name);
     filename<<name;
     filename>>name;
 
+    highscore=900-diff;
 
-    highscore = 900 - diff;
-
-    if(highscore < 0)
+    if(highscore<0)
     {
         message1="You already crossed the Time limit i.e., 900 seconds";
         filename<<message1;
         ++lost;
     }
-    else if(highscore > 450 && highscore < 800)
+    else if(highscore>450 && highscore<800)
     {
         message1="GOOD";
         filename<<message1;
         ++won;
 
     }
-    else if(highscore > 800)
+    else if(highscore>800)
     {
         message1="Outstanding";
         filename<<message1;
         ++won;
 
     }
-    else if(highscore < 200)
+    else if(highscore<200)
     {
         message1="Bad";
         filename<<message1;
@@ -70,5 +74,34 @@ void Player::player_input(double diff)
     filename.close();
 
     
+}
 
+void file::recall()
+{
+    string name2;
+
+    cout<<"Enter Your Name"<<endl;
+    getline(cin,name2);
+
+    if (filename.is_open()) 
+    {
+              if (!filename.eof()) 
+              {
+                  if(name==name2)
+                  {
+                      filename.read((char*)&name2, sizeof(name2));
+                      cout << "\n Player Name:\t" << name;
+                       cout << "\n WON" <<won;
+                      cout << "\n LOST \t" <<lost;
+                      cout << "\n HIGHSCORE \t"<<highscore;
+                      
+                   }
+              }
+    }
+
+    else
+    {
+       void player_input();
+    }
+                  
 }

af03955b7a676f493fc692f9f68f7dae496c181c
kaustubh0777 <59095728+kaustubh0777@users.noreply.github.com>
2020-11-21 13:28:59 +0530

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -7,43 +7,56 @@
 using namespace std;
 using namespace user_gm;
 
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
 class file
 {
     public:
     fstream filename;
     string message="Welcome to the Sudoku Game";
-    string name;
+    char name[30];
     double highscore;
     string message1;
     string message2="WON,LOST,HIGHSCORE";
     int won=0;
     int lost=0;
-    void player_input();
-    void recall();
-    
+    int diff=50;
+    void filewrite();
+    void fileread();
+
 };
-void file::player_input()
+void file::filewrite()
 {
    
-  filename.open("Sudoku_file.txt",ios::app | ios::in | ios::out);
+  filename.open("Player_record.txt",ios::app | ios::in | ios::out);
 
     if(!filename)
     {
         cout<<"No File is Created "<<endl;
     }
-
+    else
+    {
+        cout<<"File created successfully"<<endl;
+    }
+    
     filename<<message;
     filename>>message;
 
+
     if(filename)
-    cout<<"Enter the Player Name "<<endl;
-    getline(cin,name);
+    
     filename<<name;
     filename>>name;
 
+    cout<<name;
+
     highscore=900-diff;
 
-    if(highscore<0)
+   if(highscore<0)
     {
         message1="You already crossed the Time limit i.e., 900 seconds";
         filename<<message1;
@@ -70,38 +83,44 @@ void file::player_input()
         ++won;
     }
     filename<<message2<<won<<lost<<highscore;
+    filename>>message2>>won>>lost>>highscore;
+
     
     filename.close();
 
     
 }
 
-void file::recall()
+void file::fileread()
 {
-    string name2;
 
-    cout<<"Enter Your Name"<<endl;
-    getline(cin,name2);
+    char name2[30];
+     cout<<"Enter Your Name"<<endl;
+     cin.getline(name2,30);
 
-    if (filename.is_open()) 
-    {
-              if (!filename.eof()) 
-              {
-                  if(name==name2)
-                  {
-                      filename.read((char*)&name2, sizeof(name2));
-                      cout << "\n Player Name:\t" << name;
-                       cout << "\n WON" <<won;
-                      cout << "\n LOST \t" <<lost;
-                      cout << "\n HIGHSCORE \t"<<highscore;
+     if(name==name2)
+     {
+         filename.read((char*)&name2, sizeof(name2));
+         cout << "\n Player Name:\t" << name;
+         cout << "\n WON" <<won;
+         cout << "\n LOST \t" <<lost;
+         cout << "\n HIGHSCORE \t"<<highscore;
                       
-                   }
-              }
     }
-
-    else
+    
+   else
     {
-       void player_input();
+       cout<<"Creating new Player Record"<<endl;
+       fileread();
+       filename.close();
     }
                   
+}
+int main()
+{
+    file obj;
+    
+    obj.fileread();
+
+
 }
